1c3f4376d888ff5fbb68efda04f21fa9814d27fe
sibmurzik <marrisol@yandex.ru>
2020-06-17 17:10:33 +0700

    hw items 3

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.model;
 
+import ru.javawebinar.topjava.web.SecurityUtil;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -13,6 +15,8 @@ public class Meal  extends AbstractBaseEntity {
 
     private final int calories;
 
+    private final int userId;
+
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this(null, dateTime, description, calories);
     }
@@ -22,6 +26,13 @@ public class Meal  extends AbstractBaseEntity {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+        this.userId = SecurityUtil.authUserId();
+
+
+    }
+
+    public int getUserId() {
+        return userId;
     }
 
     public Integer getId() {
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -3,11 +3,12 @@ package ru.javawebinar.topjava.repository.inmemory;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
     private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
@@ -30,17 +31,28 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public boolean delete(int id) {
-        return repository.remove(id) != null;
+        if (repository.get(id).getUserId() == SecurityUtil.authUserId()) {
+            return repository.remove(id) != null;
+        }
+        else return false;
     }
 
     @Override
     public Meal get(int id) {
-        return repository.get(id);
+        if (repository.get(id).getUserId() == SecurityUtil.authUserId()) {
+            return repository.get(id);
+        }
+        else return null;
     }
 
     @Override
     public Collection<Meal> getAll() {
-        return repository.values();
+
+       return  repository.values().stream().filter(meal -> meal.getUserId()==SecurityUtil.authUserId()).
+               sorted((o1, o2)-> o2.getDateTime().compareTo(o1.getDateTime())).collect(Collectors.toCollection(ArrayList::new));
+
+
+
     }
 }
 
